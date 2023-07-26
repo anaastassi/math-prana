49e540cf7378cccdb947617ef9d3f3777a66375a
Vel <avior@gmail.com>
2023-07-26 16:01:20 +0300

    bugfix 1.0.2.5

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
-    if( this.scrollY > 0){
+    if( this.scrollY > 120){
         document.getElementById('headerMenu').style.backgroundColor = 'white';
     }
-    else if(this.scrollY === 0){
+    else if(this.scrollY <= 120){
         document.getElementById('headerMenu').style.background = 'none';
     }})
