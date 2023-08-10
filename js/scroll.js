306257959afeb89beb794a90c8715bb3eb36870c
Vel <avior@gmail.com>
2023-08-10 13:17:25 +0300

    reconstruction

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    if( this.scrollY > 120){
-        document.getElementById('headerMenu').style.backgroundColor = 'white';
-    }
-    else if(this.scrollY <= 120){
-        document.getElementById('headerMenu').style.background = 'none';
-    }})
