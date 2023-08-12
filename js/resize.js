31d02bbe3d22c19e6b6d9ddcd79520b872a9a4dd
anaastassi <101422994+anaastassi@users.noreply.github.com>
2023-08-12 23:20:33 +0300

    2

--- js/resize.js
+++ js/resize.js
@@ -12,7 +12,7 @@
 function updateText({page, scale}){
     if($(page).length > 0) {
         console.log(scale);
-        if(page.tagName == "A" || page.tagName == "H1" || page.tagName == "LABEL" || page.tagName == "BUTTON" || page.tagName == "H5" || page.tagName=="H2" || page.tagName=="SPAN" || page.tagName=="P"){
+        if(page.tagName == "A" || page.tagName == "H1" || page.tagName == "LI" ||page.tagName == "INPUT" || page.tagName == "LABEL" || page.tagName == "BUTTON" || page.tagName == "H5" || page.tagName=="H2" || page.tagName=="SPAN" || page.tagName=="P"){
             $(page).css("font-size", "");
             let fontSize = parseInt($(page).css("font-size"), 10) * (scale * scale);
             $(page).css("font-size", fontSize+"px");
