cffa5c1bdad6730a8cb205223a09b709526f9693
anaastassi <101422994+anaastassi@users.noreply.github.com>
2023-08-12 23:05:49 +0300

    2

--- js/resize.js
+++ js/resize.js
@@ -12,9 +12,12 @@
 function updateText({page, scale}){
     if($(page).length > 0) {
         console.log(scale);
-        $(page).css("font-size", "");
-        let fontSize = parseInt($(page).css("font-size"), 10) * (scale * scale);
-        $(page).css("font-size", fontSize+"px");
+        if(page.tagName == "A" || page.tagName == "H1" || page.tagName=="H2" || page.tagName=="SPAN" || page.tagName=="P"){
+            $(page).css("font-size", "");
+            let fontSize = parseInt($(page).css("font-size"), 10) * (scale * scale);
+            $(page).css("font-size", fontSize+"px");
+        }
+        
         for (let i = 0; i < $(page).children().length; i++) {
             const branch = $(page).children()[i];
             updateText({page: branch, scale});
